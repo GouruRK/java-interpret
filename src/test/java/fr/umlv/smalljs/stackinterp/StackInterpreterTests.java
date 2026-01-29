c0b32cefdbbee6ef8a6371f820d48c0a735c74ef
GouruRK <gouru.rk.gouru.rk@gmail.com>
2026-01-29 16:08:10 +0100

    tree to bytecode

--- src/test/java/fr/umlv/smalljs/stackinterp/StackInterpreterTests.java
+++ src/test/java/fr/umlv/smalljs/stackinterp/StackInterpreterTests.java
@@ -20,7 +20,6 @@ public class StackInterpreterTests {
     return outStream.toString(StandardCharsets.UTF_8).replace("\r\n", "\n");
   }
 
-  /*
   @Nested
   public class Q2 {
     @Test
@@ -218,6 +217,7 @@ public class StackInterpreterTests {
     }
   }
 
+  /*
   @Nested
   public class Q11 {
     @Test
